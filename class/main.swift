27cce276a782c6f20be11598c2ed1e4d41f6ca72
ZelalDev <zelall2222@gmail.com>
2025-12-16 19:25:31 +0300

    code created

--- class/main.swift
+++ class/main.swift
@@ -7,5 +7,64 @@
 
 import Foundation
 
-print("Hello, World!")
+class Animal{
+    let legs:Int // number of legs an animal has
+    init(legs: Int) {
+        self.legs = legs
+    }
+}
+
+class Dog:Animal{
+    override init(legs: Int) {
+        super.init(legs: 4)
+    }
+    func speak(){
+        print("barking")
+    }
+}
+
+final class Corgi:Dog{
+    override func speak() {
+        print("Corgi barking")
+    }
+}
+
+final class Poodle:Dog{
+    override func speak() {
+        print("poodle barking")
+    }
+}
+
+class Cat:Animal{
+    let isTame:Bool
+    init(isTame: Bool) {
+        self.isTame = isTame
+        super.init(legs: 4)
+    }
+    func speak(){
+        print("miav")
+    }
+}
+
+class Persian:Cat{
+    override init(isTame: Bool) {
+        super.init(isTame: true)
+    }
+    override func speak() {
+        print("persian miav")
+    }
+}
+
+class Lion:Cat{
+    override init(isTame: Bool) {
+        super.init(isTame: false)
+    }
+    override func speak() {
+        print("lion roar")
+    }
+}
+
+
+
+
 
